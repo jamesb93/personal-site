64326b55fa8105231d9f5dc22fe86453346b7017
James Bradbury <jamesbradbury93@gmail.com>
2020-02-09 18:03:13 +0000

    use node_modules not bower

--- NPMBuild/update_deps.sh
+++ NPMBuild/update_deps.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "Copying chart.js"
-cp ./bower_components/chart.js/dist/Chart.min.js ../static/js/Chart.min.js
+cp ./node_modules/chart.js/dist/Chart.min.js ../static/js/Chart.min.js
+
 echo "Copying jquery"
-cp ./bower_components/jquery/dist/jquery.min.js ../static/js/jquery.min.js
+cp ./node_modules/jquery/dist/jquery.min.js ../static/js/jquery.min.js
+
+echo "Copying peaks.js"
+cp ./node_modules/peaks.js/peaks.js ../static/js/peaks.js
