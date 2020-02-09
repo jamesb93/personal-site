5b790029689f83b86e8c1da936a86f55939dd81e
James Bradbury <jamesbradbury93@gmail.com>
2020-02-09 17:40:23 +0000

    script for updating bower deps

---
+++ NPMBuild/update_deps.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Copying chart.js"
+cp ./bower_components/chart.js/dist/Chart.min.js ../static/js/Chart.min.js
+echo "Copying jquery"
+cp ./bower_components/jquery/dist/jquery.min.js ../static/js/jquery.min.js
